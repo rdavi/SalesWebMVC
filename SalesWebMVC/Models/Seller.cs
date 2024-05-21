c71001a2c6648b2265cac1799d480b9fba48d768
rdavi <rdavi78@gmail.com>
2024-05-21 16:48:45 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWebMVC.Models
         public double BaseSalary { get; set; }
 
         public  Department Department { get; set; } //EF Relation. MyProperty { get; set; }
+        public int DepartmentId { get; set; } //EF Relation. MyProperty { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); //EF Relation. MyProperty { get; set; }
 
         public Seller()
