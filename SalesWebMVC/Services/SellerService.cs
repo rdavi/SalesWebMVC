3520344c753d4089bd1897221e284535fc20f0c4
rdavi <rdavi78@gmail.com>
2024-05-21 16:37:15 -0300

    Simple create form

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,12 @@ namespace SalesWebMVC.Services
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
